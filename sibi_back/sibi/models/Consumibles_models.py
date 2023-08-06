b9ecf3972c94652d3b19bba7d64cd2301632974d
Jean Pierre <jeanlondono09@gmail.com>
2023-08-05 19:15:16 -0500

    Change consumibles format

--- sibi_back/sibi/models/Consumibles_models.py
+++ sibi_back/sibi/models/Consumibles_models.py
@@ -37,3 +37,6 @@ class Consumibles(models.Model):
     is_active = models.BooleanField(default=True)  
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.numero_factura)
