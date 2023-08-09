4b45ed53cdba9755f760d99f97c7280f953f60a6
Jean Pierre <jeanlondono09@gmail.com>
2023-08-08 22:08:29 -0500

    Add __str__ ubicacion model

--- sibi_back/sibi/models/Ubicacion_models.py
+++ sibi_back/sibi/models/Ubicacion_models.py
@@ -6,3 +6,6 @@ class Ubicacion(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.lugar
